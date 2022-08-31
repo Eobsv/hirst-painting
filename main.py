33afc190384f9aada62cc6372c3a78d0bef2f1cd
Eobsv <d.eobsv@gamail.com>
2022-08-31 13:55:11 +0200

    Did all things to repair stuff. It's working with fixed conditions.

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ rgb_colors = []
 colors = colorgram.extract('image.jpg', 7 * 12)
 screen = t.Screen()
 screen.screensize(canvwidth=250/2, canvheight=250/2)
-# screen.window_width()
-# screen.window_height()
 screen.colormode(255)
 tim.setpos(0, 0)
 tim.pensize(10)
 tim.penup()
+tim.speed(100)
 
 
 # for color in colors:
@@ -23,19 +22,18 @@ tim.penup()
 #     new_color = (r, g, b)
 #     rgb_colors.append(new_color)
 
-color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123),
-              (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35),
-              (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77),
-              (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64),
-              (107, 127, 153), (176, 192, 208), (168, 99, 102), (66, 64, 60), (219, 178, 183), (178, 198, 202),
-              (112, 139, 141), (254, 194, 0)]
+color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243),
+              (149, 75, 50), (222, 201, 136), (53, 93, 123),
+              (170, 154, 41), (138, 31, 20), (134, 163, 184),
+              (197, 92, 73), (47, 121, 86), (73, 43, 35),
+              (145, 178, 149), (14, 98, 70), (232, 176, 165),
+              (160, 142, 158), (54, 45, 50), (101, 75, 77),
+              (183, 205, 171), (36, 60, 74), (19, 86, 89),
+              (82, 148, 129), (147, 17, 19), (27, 68, 102),
+              (12, 70, 64), (107, 127, 153), (176, 192, 208),
+              (168, 99, 102), (66, 64, 60), (219, 178, 183),
+              (178, 198, 202), (112, 139, 141), (254, 194, 0)]
 
-print(color_list[0])
-print(len(color_list))
-print("jestem przed randem")
-random.randint(0, len(color_list))
-print(color_list[random.randint(0, len(color_list))])
-print("jestem tu, przed petla")
 for i in range(-5, 5):
     print(tim.position())
     tim.penup()
@@ -50,4 +48,5 @@ for i in range(-5, 5):
         tim.dot()
         tim.penup()
     tim.penup()
+
 screen.exitonclick
